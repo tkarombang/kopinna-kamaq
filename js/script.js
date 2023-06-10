e875fd2e38247f6d4ed2b7f58e0036208ff02118
tkarombang <tuangkarombang@gmail.com>
2023-06-11 05:34:54 +0800

    ke-2 update landing page

--- js/script.js
+++ js/script.js
@@ -1,14 +1,84 @@
-// Toggle class active
+// Tombol Search Start
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+document.querySelector('#search-icon').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+};
+// Tombol Search End
+
+
+// Toggle Class Active Hamburger Menu Start
 const navbarNav = document.querySelector('.navbar-nav');
-const hamburgerMenu = document.querySelector('#hamburger-menu');
-// ketika hamburger-menu di click
-hamburgerMenu.onclick = () => {
+
+// Ketika Hamburger-Menu di-click
+document.querySelector('#hamburger-menu').onclick = () => {
   navbarNav.classList.toggle('active');
 };
 
-// click diluar sidebar u menghilangkan NAV
+// Toggle Class Active Hamburger Menu End
+
+// clik di luar Elemen Start
+const hm = document.querySelector('#hamburger-menu');
+const si = document.querySelector('#search-icon');
+const sc = document.querySelector('.shopping-cart');
+
 document.addEventListener('click', function (e) {
-  if (!hamburgerMenu.contains(e.target) && !navbarNav.contains(e.target)) {
+  // kondisi HAMBURGER MENU DAN NAVBAR
+  if (!hm.contains(e.target) && (!navbarNav.contains(e.target))) {
     navbarNav.classList.remove('active');
   }
+
+  // kondisi SEARCH ICON DAN SEARCH-FORM
+  if (!si.contains(e.target) && (!searchForm.contains(e.target))) {
+    searchForm.classList.remove('active');
+  }
+
+  // kondisi SHOPPING CART DAN CART ITEM
+  if (!sc.contains(e.target) && (!cartItem.contains(e.target))) {
+    sc.classList.remove('active-cart');
+    cartItem.classList.remove('active-box');
+  }
 });
+
+// clik di luar Elemen End
+
+
+// Shopping Cart Active Start
+const cartItem = document.querySelector('.cart-box');
+
+document.querySelector('#shopping-cart').onclick = (e) => {
+  sc.classList.toggle('active-cart');
+  cartItem.classList.toggle('active-box');
+  e.preventDefault();
+}
+document.addEventListener('click', function (e) {
+
+})
+// Shopping Cart Active End
+
+
+// Modal Box Start
+const modal = document.querySelector('#item-detail-modal');
+const btnDetailProducts = document.querySelectorAll('.btn-detail-product');
+
+btnDetailProducts.forEach((btn) => {
+  btn.onclick = (e) => {
+    modal.style.display = 'flex';
+    e.preventDefault();
+  };
+});
+
+// klik tombol Close
+document.querySelector('.modal-btn-close').onclick = (e) => {
+  modal.style.display = 'none';
+  e.preventDefault();
+}
+// klik modal
+window.onclick = (e) => {
+  if (e.target === modal) {
+    modal.style.display = 'none';
+  }
+}
+// Modal Box End
